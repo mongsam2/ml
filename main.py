621d04c2b1b2735e1deffef8aff02a5c9465e72c
mongsam2 <songjaehyun121476@gmail.com>
2025-11-23 21:22:00 +0900

    chore: add description in streamlit demo

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ data1 = load_data("data1")
 data2 = load_data("data2")
 total_data = pd.concat([data1, data2])
 st.title("Data Visualization")
+st.write(
+    "**category**: data label    \n"
+    "**data_id**: 1.txt, 2.txt, etc. each represent a single time series data.   \n"
+    "**time_step**: It represents the time axis in a single data set"
+)
 st.dataframe(total_data)
 
 category = st.pills("Select Category", options=CATEGORY_LIST)
